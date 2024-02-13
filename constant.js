63b4a5812400e90ed2a62f060a4b3ace00763875
Muhammed Mubashir N <mubashirnanath@gmail.com>
2024-02-13 16:01:25 +0530

    initial commit

---
+++ constant.js
@@ -0,0 +1,55 @@
+const books = [
+    {
+      id: 1,
+      name: "The Great Gatsby",
+      authorId: 1,
+    },
+    {
+      id: 2,
+      name: "To Kill a Mockingbird",
+      authorId: 2,
+    },
+    {
+      id: 3,
+      name: "1984",
+      authorId: 3,
+    },
+    {
+      id: 4,
+      name: "The Catcher in the Rye",
+      authorId: 2,
+    },
+    {
+      id: 5,
+      name: "Pride and Prejudice",
+      authorId: 2,
+    },
+    {
+      id: 6,
+      name: "Animal Farm",
+      authorId: 3,
+    },
+    {
+      id: 7,
+      name: "The Diary of Anne Frank",
+      authorId: 3,
+    },
+  ];
+  
+  // authors
+   const authors = [
+      {
+        id: 1,
+        name: 'F. Scott Fitzgerald'
+      },
+      { 
+        id: 2,
+        name: 'Harper Lee'
+      },
+      {
+        id: 3, 
+        name: 'George Orwell'
+      },
+    ];
+
+    module.exports = {books,authors}
